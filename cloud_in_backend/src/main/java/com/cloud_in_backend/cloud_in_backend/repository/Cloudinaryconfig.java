cce21ec77baf8d4f097ffadc8d1052a27377b19e
quantambites <158439988+quantambites@users.noreply.github.com>
2025-06-27 22:41:17 +0530

    Update Cloudinaryconfig.java

--- cloud_in_backend/src/main/java/com/cloud_in_backend/cloud_in_backend/repository/Cloudinaryconfig.java
+++ cloud_in_backend/src/main/java/com/cloud_in_backend/cloud_in_backend/repository/Cloudinaryconfig.java
@@ -13,9 +13,9 @@ public class Cloudinaryconfig {
     public Cloudinary cloudinary(){
         return new Cloudinary(ObjectUtils.asMap(
 
-                "cloud_name","dvdnuar8m",
-                "api_key","942187859242158",
-                "api_secret","1X6Uotwcp7nKxwXX6xzInnJW3YM"
+                "cloud_name","d********",
+                "api_key","************",
+                "api_secret","**********"
         ));
     }
 }
